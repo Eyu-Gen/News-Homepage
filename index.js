97e5c400daef68b4888056af85af86d111594792
Eyu-Gen <dreamersthebig@gmail.com>
2024-06-13 16:34:14 +0545

    Completed

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 document.getElementById("menuBtn").addEventListener("click", function() {
+    (document.getElementById("navSideBar")).style.display = "block";
+    (document.getElementById("navSideBar")).style.touchAction = "auto";
     (document.getElementById("navSideBar")).style.right = "0";
     (document.getElementById("overlay")).style.display = "block";
     document.body.style.overflow = "hidden";
@@ -6,5 +8,7 @@ document.getElementById("menuBtn").addEventListener("click", function() {
     
     document.getElementById("menuCloseBtn").addEventListener("click", function() {
         (document.getElementById("navSideBar")).style.right = "-70%";
+        (document.getElementById("navSideBar")).style.touchAction = "none";
         (document.getElementById("overlay")).style.display = "none";
+        document.body.style.overflow = "scroll";
 });
